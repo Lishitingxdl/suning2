9505223289ea25bffda0e09d4eae3b33a26ad832
Lishitingxdl <1289873071@qq.com>
2018-04-03 14:08:55 +0800

    1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 {
 	let imgs=document.querySelectorAll(".banner_img img");
 	let pagers=document.querySelectorAll(".pagerbox li");
-	let banner=document.querySelector(".banner");
+	let banner=document.querySelector(".banner_item");
 	let next=document.querySelector(".next");
 	let prev=document.querySelector(".prev");
 	// console.dir(img[0])
@@ -13,7 +13,7 @@
 				pagers[i].classList.remove("active");
 			}
 			this.classList.add("active");
-			// imgs[index].classList.add("active");
+			imgs[index].classList.add("active");
 			n=index;
 		}
 	})
@@ -32,10 +32,10 @@
 		}
 		for(let i=0;i<imgs.length;i++){
 				imgs[i].classList.remove("active");
-				// pagers[i].classList.remove("active");
+				pagers[i].classList.remove("active");
 			}
 		imgs[n].classList.add("active");
-		// pagers[n].classList.add("active");
+		pagers[n].classList.add("active");
 		}
 	banner.onmouseenter=function(){           //鼠标进入暂停
 		clearInterval(t);                     //清除自动
@@ -63,6 +63,22 @@
 		})
 	})
 }
+//nav
+{
+	let lis=document.querySelectorAll(".banner_nav li");
+	let menus=document.querySelectorAll(".menu");
+	let obj=menus[0];
+	lis.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			obj.style.display="none";
+			menus[index].style.display="block";
+			obj=menus[index];
+		}
+		ele.onmouseleave=function(){
+			menus[index].style.display="none";
+		}
+	})
+}
 //onscroll事件
 {
 	let topbar=document.querySelector("#topbar");
@@ -85,6 +101,22 @@
 {
 	let totop=document.querySelector(".leftbar_top");
 	totop.onclick=function(){
+		// document.documentElement.scrollTop=0;
+		let st=document.documentElement.scrollTop;
+		let t=setInterval(function(){  
+			st-=200;      //固定速度
+			if(st<0){
+				st=0;
+				clearInterval(t); //把当前时间函数停止
+			}
+			document.documentElement.scrollTop=st;
+		},25);
+	}
+}
+//右侧top
+{
+	let totop1=document.querySelector(".totop");
+	totop1.onclick=function(){
 		// document.documentElement.scrollTop=0;
 		let st=document.documentElement.scrollTop;
 		let t=setInterval(function(){  
@@ -142,8 +174,57 @@
 	})
 }
 //大惠聚
-
-
-
+{
+        const prev=document.querySelector(".dahuiju_prev");
+        const next=document.querySelector(".dahuiju_next");
+        const inner=document.querySelector(".dajuhui_inner");
+        let n=1;
+        let flag=true;
+        next.onclick=function(){
+            if(flag){
+                flag=false;
+                n++;
+                inner.style.transition="all 0.5s";
+                inner.style.marginLeft=-1000*n+"px";
+            }
+        };
+        prev.onclick=function(){
+            if(flag){
+                flag=false;
+                n--;
+                inner.style.transition="all 0.5s";
+                inner.style.marginLeft=-1000*n+"px";
+            }
+        };
+        inner.addEventListener("transitionend",function(){
+            flag=true;
+            if(n===4){
+                inner.style.transition="none";
+                inner.style.marginLeft="-1000px";
+                n=1;
+            }
+            if(n===0){
+                inner.style.transition="none";
+                inner.style.marginLeft="-3000px";
+                n=3;
+            }
+        })
+}
 
 //排行榜
+
+
+
+
+//banner左侧
+// {
+//     $(".banner_nav1")
+//         .mouseenter(function () {
+//             let index = $(this).index(".banner_nav1");
+//             $(".menu").eq(index).show();
+//         })
+//         .mouseleave(function () {
+//             let index = $(this).index(".banner_nav1");
+//             $(".menu").eq(index).hide();
+//         })
+// }
---
+++ js/sn.js
@@ -0,0 +1,6 @@
+/*
+* @Author: lenovo
+* @Date:   2018-03-31 16:53:43
+* @Last Modified by:   lenovo
+* @Last Modified time: 2018-04-02 21:04:10
+*/
